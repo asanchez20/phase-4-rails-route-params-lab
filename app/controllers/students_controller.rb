32ccfb67cba8ee007e29b1997c7a86a05f55f2f7
Alvin <alvin.sanchez27@gmail.com>
2022-10-12 01:48:04 -0400

    commit tree 34b44a992114db16c58db30d8d78ea5925bfa824

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,9 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def find
+    student = Student.find(params[:id])
+    render json: student
+  end
 
 end
